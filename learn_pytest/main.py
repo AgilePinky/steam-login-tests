e5706d3d2e0af1161cadde9d38e015bd04e33a54
Ivan Sharipov <i.sharipov@trueengineering.ru>
2026-01-28 17:15:54 +0700

    Edit EC and gitignore

--- learn_pytest/main.py
+++ learn_pytest/main.py
@@ -9,13 +9,16 @@ MAIN_URL = "https://store.steampowered.com/"
 TIMEOUT = 10
 fake = Faker()
 
+
 class Locators:
     PAGE = (By.XPATH, "//div[@id='global_header']")
     LOGIN_BUTTON = (By.XPATH, "//a[contains(@class, 'global_action_link')]")
-    LOGIN_FIELD = (By.XPATH,"//div[contains(@data-featuretarget, 'login')]//input[contains(@type, 'text')]")
-    PASSWORD_FIELD = (By.XPATH,"//input[contains(@type, 'password')]")
-    ENTER_BUTTON = (By.XPATH,"//div[contains(@class, 'login_featuretarget_ctn')]//button")
-    ERROR_TEXT = (By.XPATH,"//div[contains(@class, 'login_featuretarget_ctn')]//form//div[5]")
+    LOGIN_FIELD = (By.XPATH, "//div[contains(@data-featuretarget, 'login')]//input[contains(@type, 'text')]")
+    PASSWORD_FIELD = (By.XPATH, "//input[contains(@type, 'password')]")
+    ENTER_BUTTON = (By.XPATH, "//div[contains(@class, 'login_featuretarget_ctn')]//button")
+    ERROR_TEXT = (By.XPATH, "//div[contains(@class, 'login_featuretarget_ctn')]//form"
+                  "//div[5][contains(text(), 'Пожалуйста, проверьте свой пароль и имя аккаунта и попробуйте снова.')]")
+
 
 @pytest.fixture
 def driver():
@@ -23,9 +26,11 @@ def driver():
     yield driver
     driver.quit()
 
+
 @pytest.fixture
 def wait(driver):
     return WebDriverWait(driver, TIMEOUT)
+
 
 def test_sign_in(driver, wait):
     driver.get(MAIN_URL)
@@ -46,8 +51,8 @@ def test_sign_in(driver, wait):
     enter_button = wait.until(EC.element_to_be_clickable(Locators.ENTER_BUTTON))
     enter_button.click()
 
-    wait.until(EC.text_to_be_present_in_element(Locators.ERROR_TEXT, 'П'))
-    error_element = wait.until(EC.visibility_of_element_located(Locators.ERROR_TEXT))
+    error_element = wait.until(EC.presence_of_element_located(Locators.ERROR_TEXT))
     expected_text = 'Пожалуйста, проверьте свой пароль и имя аккаунта и попробуйте снова.'
 
-    assert error_element.text == expected_text, "Сообщение об ошибке отображается"
+    assert error_element.text == expected_text, (f"Ожидалось: {expected_text}, \n"
+                                                 f"Фактический результат: {error_element.text}\n")
