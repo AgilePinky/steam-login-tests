5bd94a570aec517554df022760af34b90fe83dd7
Ivan Sharipov <i.sharipov@trueengineering.ru>
2026-02-04 14:58:38 +0700

    Stable version

--- learn_pytest/pages/home_page.py
+++ learn_pytest/pages/home_page.py
@@ -1,58 +1,33 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import search_page
 
 class HomePage:
-    instance = None
-    driver = None
+    _instance = None
 
-    def __new__(cls):
-        if cls.instance is None:
-            cls.instance = super().__new__(cls)
-        return cls.instance
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
 
-    def __init__(self, driver=None):
-        if self.driver is None and driver is not None:
+    def __init__(self, driver=None, timeout=10):
+        if not hasattr(self, 'driver') or driver is not None:
             self.driver = driver
-            self.wait = WebDriverWait(driver, 10)
-        elif HomePage.driver is not None:
-            self.driver = driver
-            self.wait = WebDriverWait(driver, 10)
+            self.wait = WebDriverWait(self.driver, timeout) if driver else None
 
-    HOME_PAGE_URL = 'https://store.steampowered.com/'
+    SEARCH_FIELD = (By.XPATH, "//input[contains(@type, 'text')]")
+    SEARCHING_BUTTON = (By.XPATH, "//button[contains(@type, 'submit')]")
 
-    SEARCH_AREA = (By.XPATH, "//form[contains(@role, 'search')]//div//input")
-    SEARCH_BUTTON = (By.XPATH, "//form[contains(@role, 'search')]//button")
-    SORT_BY_TRIGGER = (By.XPATH, "//button[contains(@id, 'sort_by_trigger')]")
-    HIGHLIGHTED_SELECTION = (By.XPATH, "//a[contains(@class, 'highlighted_selection')]")
-
-    def open_page(self):
-        self.driver.get(self.HOME_PAGE_URL)
+    def click_on_search_field(self):
+        self.wait.until(EC.element_to_be_clickable(self.SEARCH_FIELD)).click()
         return self
 
-    def click_search_area(self):
-        self.wait.until(EC.element_to_be_clickable(self.SEARCH_AREA)).click()
+    def input_request(self, name_of_game):
+        search_field = self.wait.until(EC.element_to_be_clickable(self.SEARCH_FIELD))
+        search_field.clear()
+        search_field.send_keys(name_of_game)
         return self
 
-    def enter_request(self, name_of_game):
-        search_area = self.wait.until(EC.element_to_be_clickable(self.SEARCH_AREA))
-        search_area.clear()
-        search_area.send_keys(name_of_game)
-        return self
-
-    def click_search_button(self):
-        self.wait.until(EC.element_to_be_clickable(self.SEARCH_BUTTON)).click()
-        return search_page.SearchPage(self.driver)
-
-    def click_sort_by_trigger(self):
-        self.wait.until(EC.element_to_be_clickable(self.SORT_BY_TRIGGER)).click()
-        return self
-
-    def choose_highlighted_selection(self):
-        self.wait.until(EC.element_to_be_clickable(self.HIGHLIGHTED_SELECTION)).click()
-        return self
-
-    def get_game_list(self, quantity):
-        for i in range(quantity):
-            print
+    def click_searching_button(self, searching_page):
+        self.wait.until(EC.element_to_be_clickable(self.SEARCHING_BUTTON)).click()
+        return searching_page
--- learn_pytest/pages/search_page.py
+++ learn_pytest/pages/search_page.py
@@ -3,5 +3,36 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class SearchPage:
-    def __init__(self, driver):
-        pass
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self, driver=None, timeout=10):
+        if not hasattr(self, 'driver') or driver is not None:
+            self.driver = driver
+            self.wait = WebDriverWait(self.driver, timeout) if driver else None
+
+    TRIGGER_FILTER_MENU = (By.XPATH, "//button[contains(@class, 'trigger')]")
+    OPTION_PRICE_DESK = (By.XPATH, "//a[contains(@id, 'Price_DESC')]")
+    OPTIONS_MENU = (By.XPATH, "//ul[contains(@id, 'sort_by_droplist')]")
+    ALL_GAMES = (By.XPATH, "//span[@class='title']")
+    DYNAMIC_SEARCHING_LABEL = (By.XPATH, "//div[contains(@class, 'tag_dynamic')]//span[contains(@class, 'label')]")
+    DYNAMIC_SEARCHING_RESULTS = (By.XPATH, "//div[contains(@id, 'search_results_filtered_warning_persistent')]")
+
+    def click_sort_dropdown(self):
+        self.wait.until(EC.element_to_be_clickable(self.TRIGGER_FILTER_MENU)).click()
+        return self
+
+    def choose_option_price_desc(self):
+        self.wait.until(EC.visibility_of_element_located(self.OPTIONS_MENU))
+        self.wait.until(EC.element_to_be_clickable(self.OPTION_PRICE_DESK)).click()
+        return self
+
+    def get_n_games(self, n=10):
+        self.wait.until(EC.visibility_of_element_located(self.DYNAMIC_SEARCHING_RESULTS))
+        self.wait.until(EC.element_to_be_clickable(self.ALL_GAMES))
+        n_games = self.wait.until(EC.presence_of_all_elements_located(self.ALL_GAMES))[:n]
+        return n_games
